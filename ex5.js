6f7c7d34d9ffb05ceb658ae44135a2d8d2eb15f0
DSM5 <fatec-dsm5@academico.intra>
2024-03-06 19:13:41 -0300

    V3

--- ex5.js
+++ ex5.js
@@ -18,3 +18,28 @@ var resultadoDobro = calcularDobro(numero);
 console.log("O dobro do número " + numero + " é: " + resultadoDobro);
 
 //Ex4
+let num1 = 7;
+let num2 = 5;
+let num3 = 8;
+
+function media(num1, num2, num3){
+    return (num1 + num2 + num3)/3;
+}
+
+//Ex5
+let numero1 = 9;
+let numero2 = 5;
+
+function numeroMaior(numero1, numero2){
+    if (numero1 > numero2){
+        return numero1;
+    }
+    return numero2;
+}
+
+//Ex6
+let num4 = 8;
+
+function produto(num4){
+    return num4 * num4;
+}
